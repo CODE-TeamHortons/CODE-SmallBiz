594c0675ab8cb6c9bf227c2f6fca733c0792e1a6
Makoz <chenhao.john.hsu@gmail.com>
2015-02-20 23:26:43 -0800

    Rest of files

--- SmallBiz/app/controllers/querys_controller.rb
+++ SmallBiz/app/controllers/querys_controller.rb
@@ -6,5 +6,11 @@ class QuerysController < ApplicationController
 
 	def create
 		render plain: params[:query].inspect
+		#pull info of rank_g ie. rank greater than 6
+		# query the RecPlaces table 
+		
+		#present a view 
+		
+		
 	end
 end
--- SmallBiz/db/seeds.rb
+++ SmallBiz/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'open-uri'
+
+=begin
+Country.delete_all
+open("Country.txt") do |countries|
+	countries.read.each_line do |data|
+		code,name = data.chomp.split(",")
+		Country.create!(:code =>code, :name=> name)
+		end
+end
+=end
+#RecPlace
+##
+
+RecPlace.delete_all
+open("Sample.txt") do |recplaces|
+	recplaces.read.each_line do |data|
+		name,age,rating = data.chomp.split(",")
+		RecPlace.create!(:name => name, :age => age, :rating =>rating)
+	end
+end
+
+=begin
+RecPlace.create(name: 'ChezAustin', age: 13, rating: 5)
+RecPlace.create(name: 'Jabba1', age: 13, rating: 6)
+RecPlace.create(name: 'Jabba2', age: 13, rating: 7)
+RecPlace.create(name: 'Jabba3', age: 13, rating: 8)
+RecPlace.create(name: 'Jabba4', age: 13, rating: 9)
+
+=end
