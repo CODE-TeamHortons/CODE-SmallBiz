e7c4357ef567a5afe808279dec3e98c59f294887
Makoz <chenhao.john.hsu@gmail.com>
2015-02-21 00:16:44 -0800

    Index tables appear

--- SmallBiz/app/controllers/querys_controller.rb
+++ SmallBiz/app/controllers/querys_controller.rb
@@ -1,16 +1,23 @@
 class QuerysController < ApplicationController
-
+	
+	
+	def index
+		@querys = RecPlace.all
+	end
+	
+	
 	def new
 	
 	end
 
 	def create
-		render plain: params[:query].inspect
+		
 		#pull info of rank_g ie. rank greater than 6
 		# query the RecPlaces table 
+		ranking = params[:rank]
 		
 		#present a view 
-		
+		place = RecPlace.all
 		
 	end
 end
