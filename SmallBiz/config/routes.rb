006d2571d2f76840f850707134c6dbb0df3b79ed
abmicyau <myau_@live.com>
2015-02-21 21:05:52 -0800

    update 0.1.0

--- SmallBiz/config/routes.rb
+++ SmallBiz/config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   get 'resources/index'
   
+  get 'resources', to: 'resources#index'
+  
+  get 'landingpage', to: 'landingpage#index'
+  
   resources :match
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
